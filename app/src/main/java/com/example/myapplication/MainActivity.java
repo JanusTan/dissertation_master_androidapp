a09d67ef60df11efaf50b42f955b1756155b3c9f
JTan <Your email>
2020-03-31 13:18:53 +0100

    131831032020

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.myapplication;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.media.MediaRecorder;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -22,12 +27,14 @@ public class MainActivity extends AppCompatActivity {
     Timer timer=null;
     TimerTask timerTask=null;
     boolean isPause=true;
+    MediaRecorder mediaRecorder ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextView time_show=findViewById(R.id.timeshow);
+        mediaRecorder = new MediaRecorder();
         startTimer();
 
         handler=new Handler(){
@@ -44,20 +51,32 @@ public class MainActivity extends AppCompatActivity {
         final Button button1 = (Button) findViewById(R.id.button_1);
         button1.setOnClickListener(new View.OnClickListener() {
             int flag=1;
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View v) {
 //                Toast.makeText(MainActivity.this, "You clicked"
 //                ,Toast.LENGTH_SHORT).show();
                 flag = flag+1;
                 if (flag%2  == 0&&flag!=0){
+
                     isPause=false;
-                button1.setText("press to stop");
+                button1.setText("Press to Send to Server");
+                    Intent intent = new Intent("MediaStore.Audio.Media.RECORD_SOUND_ACTION");
+                    startActivityForResult(intent,RECOED_AUDIO);
+
+//                record();
 //                Intent intent = new Intent("android.media.action.I");
                 }
 
                 else{
                     isPause=true;
-                    button1.setText("press to record");
+                    button1.setText("Press to Record");
+                    count=0;
+                    Toast.makeText(MainActivity.this, "Record sent"
+                            ,Toast.LENGTH_SHORT).show();
+                    time_show.setText("0:0");
+//                    mediaRecorder.stop();
+//                    mediaRecorder.release();
                 }
             }
 
@@ -97,6 +116,47 @@ public class MainActivity extends AppCompatActivity {
             timer.schedule(timerTask,1000,1000);
         }
     }
+
+//    @RequiresApi(api = Build.VERSION_CODES.O)
+//    private void record(){
+//        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+//        /**
+//         * mediaRecorder.setOutputFormat代表输出文件的格式。该语句必须在setAudioSource之后，在prepare之前。
+//         * OutputFormat内部类，定义了音频输出的格式，主要包含MPEG_4、THREE_GPP、RAW_AMR……等。
+//         */
+//        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+//        /**
+//         * mediaRecorder.setAddioEncoder()方法可以设置音频的编码
+//         * AudioEncoder内部类详细定义了两种编码：AudioEncoder.DEFAULT、AudioEncoder.AMR_NB
+//         */
+//        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+//        /**
+//         * 设置录音之后，保存音频文件的位置
+//         */
+//        File outputImage = new File(getExternalCacheDir(), "a.3pg");
+//        try {
+//            if (outputImage.exists()) {
+//                outputImage.delete();
+//            }
+//            outputImage.createNewFile();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        mediaRecorder.setOutputFile(outputImage);
+//
+//        /**
+//         * 调用start开始录音之前，一定要调用prepare方法。
+//         */
+//        try {
+//            mediaRecorder.prepare();
+//            mediaRecorder.start();
+//        } catch (IllegalStateException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 
 
 }
